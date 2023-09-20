0ea5d0612c1087d10f12660573b788f7af1938a9
Jonathan Hammond <jonathan.hammond@isitetv.com>
2023-09-20 12:43:17 +0100

    Add project files.

---
+++ TCPData/Data.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPData
+{
+    public static class Data
+    {
+        public static List<Employee> GetEmployees() 
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee = new Employee()
+            {
+                Id = 1,
+                FirstName = "Test",
+                LastName = "Testerson",
+                AnnualSalary=4.4m,
+                IsManager = true,
+                DepartmentId=1
+            };
+            employees.Add(employee);
+            employee = new Employee()
+            {
+                Id = 2,
+                FirstName = "Testy",
+                LastName = "Testersony",
+                AnnualSalary=1.4m,
+                IsManager = false,
+                DepartmentId=2
+            };
+            employees.Add(employee);
+            employee = new Employee()
+            {
+                Id = 3,
+                FirstName = "Testo",
+                LastName = "Testero",
+                AnnualSalary=10.4m,
+                IsManager = false,
+                DepartmentId=3
+            };
+            employees.Add(employee);
+
+            return employees;
+        }
+
+        public static List<Department> GetDepartments() 
+        {
+            List<Department> departments = new List<Department>();
+
+            Department department = new Department()
+            {
+                Id = 1,
+                LongName = "Testpartment",
+                ShortName = "TP",
+            };
+            departments.Add(department);
+            department = new Department()
+            {
+                Id = 2,
+                LongName = "Testing",
+                ShortName = "TE",
+            };
+            departments.Add(department);
+            department = new Department()
+            {
+                Id = 3,
+                LongName = "Testest",
+                ShortName = "TS",
+            };
+            departments.Add(department);
+
+            return departments;
+        }
+    }
+}
---
+++ TheLinqCompanyApplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using TCPData;
+using TCPExtensions;
+
+List<Employee> employees = Data.GetEmployees();
+
+var filteredEmployees = employees.Filter(e => e.IsManager);
+
+foreach (var employee in filteredEmployees)
+{
+    Console.WriteLine("Managers = " + employee.FirstName);
+}
+
+//with linq
+
+List<Department> departments = Data.GetDepartments();
+
+var filteredDepartments = departments.Where(d => d.Id == 1);
+
+Console.WriteLine(filteredDepartments.FirstOrDefault().LongName);
+
+//join lists
+
+var jointList = from emp in employees
+                join dept in departments on emp.DepartmentId equals dept.Id
+                select new
+                {
+                    FirstName = emp.FirstName,
+                    Department = dept.LongName,
+                    AnnualSalary = emp.AnnualSalary
+                };
+
+Console.WriteLine("joint " + jointList.FirstOrDefault().Department + jointList.FirstOrDefault().FirstName);
+
+var heightsSalary = jointList.Max(s => s.AnnualSalary);
